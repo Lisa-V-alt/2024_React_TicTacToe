c44ddcd83ab91512a6e25b2ab12fbd847a93ad77
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-18 13:17:41 +1000

    added player name input fields

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,28 @@
 import {useState} from 'react';
 
 export default function Player({name, symbol}){
+    //initial phase is false when loading page: NOT editing player name or showing input field
+    const[isEditing, setIsEditing] = useState(false); 
 
+    function handleEditClick(){
+        setIsEditing(true);
+    }
 
+    //[1] output span, OR...
+    let playerName = <span className='player-name'>{name}</span>;
+
+    //[2] ...if editing is true, output input
+    if(isEditing){
+        playerName = <input type="text" required />
+    }
 
     return(
     <li>
     <span className="player">
-        <span className='player-name'>{name}</span>
+        {playerName}
         <span className='player-symbol'>{symbol}</span>
     </span>
-    <button>Save</button>
+    <button onClick={handleEditClick}>Edit</button>
     </li>
     );
 }
