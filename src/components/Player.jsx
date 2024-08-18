031b38d9ce0c7398dab2bb9d95c778bf6670a4a0
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-18 15:32:11 +1000

    added activeplayer features, symbol handling

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-export default function Player({initialName, symbol}){
+export default function Player({initialName, symbol, isActive}){
     const [playerName, setPlayerName] = useState(initialName);
     //initial phase is false when loading page: NOT editing player name or showing input field
     const[isEditing, setIsEditing] = useState(false); 
@@ -26,7 +26,7 @@ export default function Player({initialName, symbol}){
     }
 
     return(
-    <li>
+    <li className={isActive ? 'active' : undefined}>
     <span className="player">
         {editablePlayerName}
         <span className='player-symbol'>{symbol}</span>
